4beb267fc2ee744395cf6c41a1f2574321ca87d3
Daniel <gendik38@gmail.com>
2023-04-12 16:42:47 +0200

    add empty board content

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Board from './components/Board'
 
 function App() {
   
 
   return (
-   <>
+   <div className='app min-h-screen w-screen'>
    <Navbar />
-   
-   </>
+   <Board />
+   </div>
   )
 }
 
