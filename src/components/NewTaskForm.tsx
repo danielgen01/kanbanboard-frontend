d5f28c9c5ffe5ec8fee19703a63c8c39c66483fb
Daniel <gendik38@gmail.com>
2023-04-12 17:14:26 +0200

    add darkmode properties

--- src/components/NewTaskForm.tsx
+++ src/components/NewTaskForm.tsx
@@ -5,15 +5,17 @@ const NewTaskForm = () => {
     <div className="transparent-background bg-black/50 absolute h-screen w-screen top-0 left-0">
       <div
         className="form-container bg-white fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-     min-h-[550px] w-4/5 rounded-md"
+     min-h-[550px] w-4/5 rounded-md 
+     dark:bg-dark-gray"
       >
         <div className="form-content flex flex-col h-full w-full px-5 py-5 gap-5">
-          <h1 className="font-bold text-xl">Add new Task</h1>
+          <h1 className="font-bold text-xl dark:text-white">Add new Task</h1>
           {/* First input field  */}
           <div className="input-title-container flex flex-col gap-2">
             <label
               htmlFor="input-title"
-              className="text-medium-gray font-bold text-sm"
+              className="text-medium-gray font-bold text-sm 
+              dark:text-white"
             >
               Title
             </label>
@@ -28,13 +30,15 @@ const NewTaskForm = () => {
           <div className="input-title-container flex flex-col gap-2">
             <label
               htmlFor="input-description"
-              className="text-medium-gray font-bold text-sm"
+              className="text-medium-gray font-bold text-sm
+              dark:text-white dark:bg-dark-gray"
             >
               Description
             </label>
             <textarea
               name="input-title"
-              className="border-bright-gray border-2 outline-bright-gray rounded-md h-20 px-2 py-2 text-sm resize-none"
+              className="border-bright-gray border-2 outline-bright-gray rounded-md h-20 px-2 py-2 text-sm resize-none
+               dark:bg-dark-gray"
               placeholder="e.g its always good to take a small break from working to prevent burnouts"
             />
           </div>
@@ -43,17 +47,19 @@ const NewTaskForm = () => {
           <div className="input-title-container flex flex-col gap-2">
             <label
               htmlFor="select-status"
-              className="text-medium-gray font-bold text-sm"
+              className="text-medium-gray font-bold text-sm
+              dark:text-white"
             >
               Status
             </label>
             <select
               name="select-status"
-              className="border-bright-gray border-2 outline-bright-gray rounded-md h-10 px-2 text-sm"
+              className="border-bright-gray border-2 outline-bright-gray rounded-md h-10 px-2 text-sm
+              dark:text-white dark:bg-dark-gray"
             >
-              <option>Todo</option>
-              <option>Doing</option>
-              <option>Done</option>
+              <option className="text-black dark:text-white">Todo</option>
+              <option className="text-black dark:text-white">Doing</option>
+              <option className="text-black dark:text-white">Done</option>
             </select>
           </div>
 
