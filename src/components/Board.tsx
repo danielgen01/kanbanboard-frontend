e4135eed32b99e49b4c41aa29e5078aee9cc18a5
Daniel <gendik38@gmail.com>
2023-04-13 09:18:07 +0200

    add basic board layout

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,23 +1,16 @@
 import React from "react"
-import { Kanbanbox } from "./Kanbanbox"
+import { TodoItemsColumn } from "./columns/todoItemsColumn"
+import {DoingColumn} from "./columns/DoingColumn"
+import { DoneColumn } from "./columns/DoneColumn"
 
 const Board = () => {
   return (
-    <main className="dark:bg-dark-black bg-dark-white min-h-screen  py-2 px-2 ">
-      <section className="columns grid-cols-1 grid ">
+    <main className="dark:bg-dark-black bg-dark-white min-h-screen  py-2 px-2">
+      <section className="columns grid-cols-1 grid lg:grid-cols-3">
         {/* First grid item */}
-        <div className="grid-item-1 flex flex-col gap-4">
-          <div className="headline flex items-center gap-2">
-            <div className="h-5 w-5 rounded-full bg-sky-500 dark:bg-teal-500"></div>
-            <h1 className="text-medium-gray -tracking-tighter text-md uppercase font-bold">
-              Todo (0){" "}
-            </h1>
-          </div>
-
-   <Kanbanbox    title={"Drama"} subtasksCount={0}/>
-   <Kanbanbox    title={"Drama"} subtasksCount={0}/>
-
-        </div>
+        <TodoItemsColumn />
+        <DoingColumn />
+        <DoneColumn />
       </section>
 
       {/* Render if no boxes here */}
@@ -27,6 +20,3 @@ const Board = () => {
 }
 
 export default Board
-
- 
-  
---
+++ src/components/columns/DoingColumn.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Kanbanbox } from "../Kanbanbox"
+import { MdSwipeLeft } from "react-icons/md"
+
+export const DoingColumn: React.FC = () => {
+  return (
+    <div className="grid-item-1(todoItems) flex flex-col gap-4">
+      <div className="headline flex items-center gap-2">
+        <div className="h-5 w-5 rounded-full bg-dark-purple dark:bg-dark-purple"></div>
+        <h1 className="text-medium-gray -tracking-tighter text-md uppercase font-bold">
+          Doing (0){" "}
+        </h1>
+        <div className="swipe-icon-text lg:hidden ml-auto">
+          <h1 className="">Swipe</h1>
+          <MdSwipeLeft />
+        </div>
+      </div>
+
+      <Kanbanbox title={"Drama"} subtasksCount={0} />
+      <Kanbanbox title={"ostblock"} subtasksCount={0} />
+    </div>
+  )
+}
