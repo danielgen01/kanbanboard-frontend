9f30610a227755fb0acd8ab13fa31b516e14f34b
Daniel <gendik38@gmail.com>
2023-04-13 12:02:45 +0200

    add font.bold

--- src/components/Forms/AddNewBoard/AddColInput.tsx
+++ src/components/Forms/AddNewBoard/AddColInput.tsx
@@ -9,7 +9,7 @@ export const AddColInput: React.FC<props> = ({ defaultValue }) => {
     <div className="input-element flex justify-between items-center">
       <input
         type="text"
-        className="px-5 border-2 border-bright-gray rounded-md h-12 dark:bg-dark-gray dark:border-medium-gray dark:border dark:text-white"
+        className="px-5 border-2 border-bright-gray rounded-md h-12 dark:bg-dark-gray dark:border-medium-gray dark:border dark:text-white font-bold"
         placeholder="e.g 2"
         defaultValue={defaultValue}
       />
