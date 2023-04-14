259a7babb5e4ab0838d99173b9bf839ca723d264
Daniel <gendik38@gmail.com>
2023-04-14 13:34:41 +0200

    add hover animation for buttons

--- src/components/Forms/DeleteTask/DeleteTask.tsx
+++ src/components/Forms/DeleteTask/DeleteTask.tsx
@@ -3,7 +3,7 @@ import React from "react"
 const DeleteTask = () => {
   return (
     <section className="transparent-background bg-black/50 absolute top-0 left-0 h-screen w-screen">
-      <div className="delete-box px-5 min-h-[300px] bg-white 
+      <div className="delete-box px-5 min-h-[250px] bg-white 
        dark:bg-dark-gray w-[90%] md:w-[50%] lg:w-[40%] xl:w-[30%] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md">
         <div className="content py-5 px-5 flex flex-col gap-5">
           <h1
@@ -17,16 +17,16 @@ const DeleteTask = () => {
             Are you sure you want to delete the tasktitle task and its subtasks?
             This action cannot be reversed.
           </p>
-          <div className="button flex flex-col gap-3">
+          <div className="button flex flex-col gap-3 lg:grid lg:grid-cols-2">
             <button
-              className="bg-dark-red text-white font-bold py-2 px-4 rounded-3xl"
+              className="bg-dark-red text-white font-bold py-2 px-4 rounded-3xl hover:bg-bright-red duration-100"
               type="button"
               onClick={() => window.location.reload()}
             >
               Delete
             </button>
             <button
-              className=" bg-bright-gray dark:bg-white text-dark-purple font-bold py-2 px-4 rounded-3xl"
+              className=" bg-bright-gray dark:bg-white text-dark-purple font-bold py-2 px-4 rounded-3xl dark:hover:bg-white"
               type="button"
               onClick={() => window.location.reload()}
             >
