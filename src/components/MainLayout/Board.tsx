043cca0f5bc90e9f911c7c61441ecfb31e50949b
Daniel <gendik38@gmail.com>
2023-04-14 16:24:43 +0200

    set col-span to MAX if sidebar isnt visible

--- src/components/MainLayout/Board.tsx
+++ src/components/MainLayout/Board.tsx
@@ -2,19 +2,27 @@ import React from "react"
 import { TodoItemsColumn } from "../columns/TodoItemsColumn"
 import { DoingColumn } from "../columns/DoingColumn"
 import { DoneColumn } from "../columns/DoneColumn"
+
+import { RootState } from "../../Redux/rootReducer"
+import { useSelector, useDispatch } from "react-redux"
+
 import Navbar from "../Navbar/Navbar"
 
 const Board = () => {
+
+  const isSideBarOpen = useSelector(
+    (state: RootState) => state.sidebar.isSideBarOpen
+  )
+
   return (
     <>
-      <main className="dark:bg-dark-black bg-dark-white min-h-screen  md:col-span-4 xl:col-span-5">
+      <main
+  className={`dark:bg-dark-black bg-dark-white min-h-screen ${
+    isSideBarOpen ? 'md:col-span-4 xl:col-span-5' : 'md:col-span-6 lg:col-span-6'
+  }`}
+>
         <Navbar />
 
-        {/* <section
-          className="columns flex flex-row gap-5  overflow-x-scroll 
-          md:grid-cols-3 md:overflow-x-hidden md:w-screen
-           md:grid grid-cols-3 md:gap-0  px-5 mt-5"
-        > */}
            <section className="columns 
            flex flex-row gap-5  overflow-x-scroll
            md:grid md:grid-cols-3  px-5 mt-5 md:overflow-x-hidden ">
