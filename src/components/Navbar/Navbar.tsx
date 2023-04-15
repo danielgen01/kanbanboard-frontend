b739394aecf268f44d0b37ab744f75751d0d9147
Daniel <gendik38@gmail.com>
2023-04-15 09:56:12 +0200

    add hover animation for add new task btn

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -34,18 +34,18 @@ const Navbar = () => {
         <figure className="logo-ctn">
           <img src={logomobile} className="h-8 md:hidden" alt="logo" />
         </figure>
-        <button className="chosen-board ml-2 flex items-center gap-2">
+        <button className="chosen-board ml-2 flex items-center gap-2 md:cursor-default">
           <h1
             className="font-bold text-lg
            dark:text-white"
           >
             Platform Launch
           </h1>
-          <img src={chevrondown} className="h-2" alt="chevron down" />
+          <img src={chevrondown} className="h-2 md:hidden" alt="chevron down" />
         </button>
         <div className="top-right-icons ml-auto flex  items-center gap-5">
           <button
-            className="add-btn flex bg-dark-purple py-3 px-5 rounded-2xl items-center gap-2 text-white font-bold"
+            className="add-btn flex bg-dark-purple py-3 px-5 rounded-2xl items-center gap-2 text-white font-bold hover:bg-bright-purple duration-150"
             onClick={handleToggleNewTaskForm}
           >
             <img src={addicon} alt="addicon" className="" />
