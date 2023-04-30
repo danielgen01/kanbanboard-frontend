bf9a5fa0aaf8691655aa9f92bc33b9ea37bcd910
Daniel <gendik38@gmail.com>
2023-04-30 08:17:35 +0200

    open editboard form if user clicks on add btn

--- src/components/reusable/EmptyBoardContent.tsx
+++ src/components/reusable/EmptyBoardContent.tsx
@@ -1,11 +1,20 @@
 import React from "react"
 import addicon from "../../../assets/icon-add-task-mobile.svg"
+import { toggleEditBoardForm } from "../../Redux/features/EditBoardForm/EditBoardFormSlice"
+import { useAppDispatch } from "../../Redux/store"
 
- const EmptyBoardContent = ()=> {
+const EmptyBoardContent = () => {
+  const dispatch = useAppDispatch()
+
+  const handleToggleEditBoardForm = () => {
+    dispatch(toggleEditBoardForm())
+  }
   return (
-    <div className="empty-board-content w-full
-     flex pt-[20%] items-center justify-center
-      flex-col ">
+    <div
+      className="empty-board-content w-full
+     flex py-[20%] items-center justify-center
+      flex-col "
+    >
       <h1 className="text-center text-medium-gray font-bold text-xl">
         This board is empty. Create a new <br />
         column to get started.
@@ -14,6 +23,7 @@ import addicon from "../../../assets/icon-add-task-mobile.svg"
         className="mt-5 text-white text-xl
          bg-dark-purple flex items-center gap-2 px-7 py-4 rounded-3xl font-bold
         "
+        onClick={handleToggleEditBoardForm}
       >
         <img src={addicon} alt="" />
         Add new Column
