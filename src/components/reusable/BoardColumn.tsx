16f44e6b6ac35647677fdfdf2840314009fa5b9e
Daniel <gendik38@gmail.com>
2023-04-30 08:34:58 +0200

    add small margin between headline and boxes

--- src/components/reusable/BoardColumn.tsx
+++ src/components/reusable/BoardColumn.tsx
@@ -32,47 +32,45 @@ export const BoardColumn: React.FC<props> = ({
     (board: any) => board.name === currentBoardName
   )
 
-
-
-
   return (
-    <div className="grid-item-2(doingItems) flex flex-col gap-4">
-      <div className="headline flex items-center gap-2">
+    <div className=" flex flex-col ">
+      <div className="headline flex items-center gap-2 mb-4">
         <div className={`h-5 w-5 rounded-full ${batchColor} `}></div>
         <h1
           className="text-medium-gray -tracking-tighter
-         text-md uppercase font-bold whitespace-nowrap"
+         text-md uppercase font-bold whitespace-nowrap
+         "
+         
         >
           {columnName} ({matchingColumn?.tasks.length}){" "}
         </h1>
       </div>
 
       {boardIndex >= 0 &&
-  data.boards[boardIndex].columns.map((column: any) => (
-    <div key={column.id} className="flex flex-col gap-3">
-      {column.tasks
-        .filter((task: any) => task.status === statusName)
-        .map((task: any) => {
-          // Berechne die Anzahl der abgeschlossenen Subtasks
-          const completedSubtasksCount = task.subtasks.filter(
-            (subtask: any) => subtask.isCompleted
-          ).length;
+        data.boards[boardIndex].columns.map((column: any) => (
+          <div key={column.id} className="flex flex-col gap-3">
+            {column.tasks
+              .filter((task: any) => task.status === statusName)
+              .map((task: any) => {
+                // Berechne die Anzahl der abgeschlossenen Subtasks
+                const completedSubtasksCount = task.subtasks.filter(
+                  (subtask: any) => subtask.isCompleted
+                ).length
 
-          return (
-            <Kanbanbox
-              key={task.name}
-              title={task.title}
-              subtasksCount={task.subtasks.length}
-              completedSubtasksCount={completedSubtasksCount} // Übergebe die Anzahl der abgeschlossenen Subtasks
-              subtasks={task.subtasks}
-              description={task.description}
-              id={task.id}
-            />
-          );
-        })}
-    </div>
-  ))}
-
+                return (
+                  <Kanbanbox
+                    key={task.name}
+                    title={task.title}
+                    subtasksCount={task.subtasks.length}
+                    completedSubtasksCount={completedSubtasksCount} // Übergebe die Anzahl der abgeschlossenen Subtasks
+                    subtasks={task.subtasks}
+                    description={task.description}
+                    id={task.id}
+                  />
+                )
+              })}
+          </div>
+        ))}
     </div>
   )
 }
