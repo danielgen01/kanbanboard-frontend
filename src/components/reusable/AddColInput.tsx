f7f62ce232c1689a517cb40757df7fc70ea99c61
Daniel <gendik38@gmail.com>
2023-05-02 08:11:27 +0200

    change cross icon to original svg

--- src/components/reusable/AddColInput.tsx
+++ src/components/reusable/AddColInput.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import crossicon from "../../../assets/icon-cross.svg"
 import { RxCross1 } from "react-icons/rx"
 type props = {
   defaultValue: string
@@ -23,12 +22,20 @@ export const AddColInput: React.FC<props> = ({
         onChange={onInputChange}
       />
 
-      <RxCross1
+      <svg
+        width="15"
+        height="15"
+        xmlns="http://www.w3.org/2000/svg"
         onClick={onRemove}
-        className="hover:text-dark-red 
-        text-dark-black font-bold cursor-pointer duration-200
+        className=" duration-150
+         font-bold cursor-pointer 
        text-3xl"
-      />
+      >
+        <g fill="#828FA3" fill-rule="evenodd" className="hover:fill-dark-red">
+          <path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z" />
+          <path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z" />
+        </g>
+      </svg>
     </div>
   )
 }
